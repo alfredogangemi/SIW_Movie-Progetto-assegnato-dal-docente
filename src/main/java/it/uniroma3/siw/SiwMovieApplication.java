d30b484a177026316818bdee841b8d48e29939ea
gangemi <a.gangemi@westpole.it>
2023-05-06 14:25:50 +0200

    first commit

---
+++ src/main/java/it/uniroma3/siw/SiwMovieApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiwMovieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SiwMovieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/uniroma3/siw/SiwMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiwMovieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
