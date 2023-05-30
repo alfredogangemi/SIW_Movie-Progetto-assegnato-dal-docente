be79585a9b982fac91cfe4340261ca1c75fc5804
gangemi <a.gangemi@westpole.it>
2023-05-30 16:11:18 +0200

    review repo interface

---
+++ src/main/java/it/uniroma3/siw/repository/ReviewRepository.java
@@ -0,0 +1,12 @@
+package it.uniroma3.siw.repository;
+
+import it.uniroma3.siw.model.Review;
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface ReviewRepository extends CrudRepository<Review, Long> {
+
+
+
+}
+
